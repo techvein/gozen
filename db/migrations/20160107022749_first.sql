570388d5b71755059dc1b101559ed263459587d6
Yasutaka Kawamoto <kwmt27@gmail.com>
2016-11-29 09:58:10 +0900

    mysql5.7対応

--- db/migrations/20160107022749_first.sql
+++ db/migrations/20160107022749_first.sql
@@ -1,21 +1,21 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 CREATE TABLE `users` (
-  `id`                INT UNSIGNED NOT NULL AUTO_INCREMENT
+  `id`            INT UNSIGNED NOT NULL AUTO_INCREMENT
   COMMENT 'ID',
-  `name`              VARCHAR(255)
+  `name`          VARCHAR(255)
   COMMENT 'ユーザー名',
-  `email`             VARCHAR(255) NOT NULL
+  `email`         VARCHAR(255)
   COMMENT 'メールアドレス',
-  `session_token`     VARCHAR(255) NOT NULL
+  `session_token` VARCHAR(255)
   COMMENT 'セッショントークン',
-  `token_expire`      TIMESTAMP    NOT NULL
+  `token_expire`  TIMESTAMP             DEFAULT CURRENT_TIMESTAMP
   COMMENT 'トークン有効期限',
-  `last_login_at`     TIMESTAMP    NOT NULL
+  `last_login_at` TIMESTAMP             DEFAULT CURRENT_TIMESTAMP
   COMMENT '最終ログイン日時',
-  `created_at`        TIMESTAMP    NOT NULL
+  `created_at`    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
   COMMENT '作成日時',
-  `updated_at`        TIMESTAMP    NOT NULL
+  `updated_at`    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
   COMMENT '更新日時',
   PRIMARY KEY (`id`),
   UNIQUE (`session_token`)
@@ -28,29 +28,21 @@ CREATE TABLE auths (
   COMMENT 'ID',
   `user_id`    INT UNSIGNED NOT NULL
   COMMENT 'ユーザーID',
-  `source`     VARCHAR(255) NOT NULL
+  `source`     VARCHAR(255)
   COMMENT '認証元',
-  `source_id`  VARCHAR(255) NOT NULL
+  `source_id`  VARCHAR(255)
   COMMENT '認証元ユーザーID',
-  `email`      VARCHAR(255) NOT NULL
+  `email`      VARCHAR(255)
   COMMENT 'メールアドレス',
-  `created_at` TIMESTAMP    NOT NULL
+  `created_at` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
   COMMENT '作成日時',
-  `updated_at` TIMESTAMP    NOT NULL
+  `updated_at` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
   COMMENT '更新日時',
   PRIMARY KEY (`id`),
   UNIQUE (`user_id`, `source`)
 )
   ENGINE = InnoDB
   CHARSET = utf8;
-
-
--- INSERT INTO `users`
--- (`created_at`, `updated_at`, `name`)
--- VALUES
---   (NOW(), NOW(), '田中'),
---   (NOW(), NOW(), '鈴木');
-
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
